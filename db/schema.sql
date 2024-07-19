15c33469215cfba2879229fd9a1ec34191e2b1e0
Michael Medina <michaeljmedina22800@gmail.com>
2024-07-19 15:29:06 -0400

    first commit

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
+
+-- CREATE DATABASE
+CREATE DATABASE ecommerce_db;
+
+CREATE TABLE Category (
+    id INT NOT NULL PRIMARY KEY SERIAL,
+    category_name VARCHAR NOT NULL
+);
+
+CREATE TABLE Product (
+    id INT NOT NULL PRIMARY KEY SERIAL,
+    product_name VARCHAR NOT NULL,
+    price DECIMAL NOT NULL,
+    stock INT NOT NULL,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES Category(id)
+);
+
+CREATE TABLE Tag (
+    id INT NOT NULL PRIMARY KEY SERIAL,
+    tag_name VARCHAR NOT NULL
+);
+
+CREATE TABLE ProductTag (
+    id INT NOT NULL PRIMARY KEY SERIAL,
+    product_id INT,
+    tag_id INT,
+    FOREIGN KEY (product_id) REFERENCES Product(id),
+    FOREIGN KEY (tag_id) REFERENCES Tag(id)
+);
