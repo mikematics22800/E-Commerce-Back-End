94fc132e5fd0373b90200bf44641eeab1c1aaf1d
Michael Medina <michaeljmedina22800@gmail.com>
2024-07-20 18:00:15 -0400

    added queries to routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,35 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  console.log('All categories:', JSON.stringify(Category.findAll(), null, 2));
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  console.log('Category by ID:', JSON.stringify(Category.findByPk(req.params.id), null, 2));
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({category_name: req.body.category_name}).then((category) => {
+    console.log(`New category ${JSON.stringify(category, null, 2)} created`);
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({category_name: req.body.category_name}, {where: {id: req.params.id}}).then((category) => {
+    console.log(`Category ${req.params.id} name changed to ${req.body.category_name}`);
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({where: {id: req.params.id}}).then((category) => {
+    console.log(`Category ${req.params.id} deleted`);
+  })
 });
 
 module.exports = router;
+
